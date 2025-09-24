24a748537104bf51d1cb20ab4e32102f3aabd960
zoeyow <140793445+zoeyow@users.noreply.github.com>
2025-09-24 14:36:51 +0800

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,4 +1,4 @@
-var vg_1 = "https://raw.githubusercontent.com/zoeyow/3179/refs/heads/main/daily_cost_bar_chart.vg.json";
+var vg_1 = "daily_cost_bar_chart.vg.json";
 vegaEmbed("#bar_chart", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
