4445327bc3de8bd7d12e1c7d1c77008ec5d66037
zoeyow <140793445+zoeyow@users.noreply.github.com>
2025-09-30 19:50:05 +0800

    Delete vega_lite_vis.js

--- vega_lite_vis.js
+++
@@ -1,4 +0,0 @@
-var vg_1 = "daily_cost_bar_chart.vg.json";
-vegaEmbed("#bar_chart", vg_1).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
